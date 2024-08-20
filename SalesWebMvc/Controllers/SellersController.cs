7eafdebe237bc624af4b4972812ce936e7bea0af
Camilo LR <autumndrums@hotmail.com>
2024-08-20 15:36:51 -0300

    Delete Seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -23,6 +23,7 @@ namespace SalesWebMvc.Controllers
             return View(list);
         }
 
+        //CREATE
         public IActionResult Create()
         {
             //Busca do banco de dados todos os departamentos
@@ -38,6 +39,31 @@ namespace SalesWebMvc.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
             //return RedirectToAction("Index");
+        }
+
+        //DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
